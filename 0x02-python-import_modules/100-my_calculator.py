c0ee7a42f0a7562c22140df21a50cd3631708550
apla02 <1873@holbertonschool.com>
2020-09-03 17:53:53 -0500

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,20 +3,24 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import(add, sub, mul, div)
     for i, j in enumerate(sys.argv[:4]):
-        if len(sys.argv) != 4:
-            print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-            exit(1)
-        else:
+        if len(sys.argv) == 4:
             a = int(sys.argv[1])
             b = int(sys.argv[3])
             if i == 2 and j == '+':
                 print("{} + {} = {}".format(a, b, add(a, b)))
+                exit(0)
             elif i == 2 and j == '-':
                 print("{} - {} = {}".format(a, b, sub(a, b)))
-            elif i == 2 and j == '*':
+                exit(0)
+            elif i == 2 and j == "*":
                 print("{} * {} = {}".format(a, b, mul(a, b)))
+                exit(0)
             elif i == 2 and j == '/':
                 print("{} / {} = {}".format(a, b, div(a, b)))
-            else:
+                exit(0)
+            elif i == 2 and j != ('+', '-', '*', '/'):
                 print('Unknown operator. Available operators: +, -, * and /')
                 exit(1)
+        else:
+            print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+            exit(1)
