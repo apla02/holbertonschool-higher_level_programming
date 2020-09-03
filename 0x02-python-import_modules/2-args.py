1005eae2dbdb1707021e4f9266d3f87e57d811c4
apla02 <1873@holbertonschool.com>
2020-09-03 12:06:14 -0500

    include

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     len_arg = len(sys.argv) - 1
     if len_arg == 0:
-        print("{} argument.".format(len_arg))
+        print("{} arguments.".format(len_arg))
     elif len_arg == 1:
         print("{} argument:".format(len_arg))
     else:
