dd0920fc4e09732eb6484dacef6b42e7618437a0
apla02 <1873@holbertonschool.com>
2020-09-03 12:20:57 -0500

    add numbers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import sys
     suma = 0
-    for i, j in enumerate(sys.argv[1:]):
+    for j in sys.argv[1:]:
         suma += int(j)
-        if i == len(sys.argv) - 2:
-            print("{}".format(suma))
+    print("{}".format(suma))
