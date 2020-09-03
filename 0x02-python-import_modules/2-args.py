926813a284da762d826ee114993699ac9f55dc8e
apla02 <1873@holbertonschool.com>
2020-09-03 11:54:33 -0500

    include the if guard to avoid executed when file is compiled

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-len_arg = len(sys.argv) - 1
-if len_arg == 0:
-    print("{} argument.".format(len_arg))
-elif len_arg == 1:
-    print("{} argument:".format(len_arg))
-else:
-    print("{} arguments:".format(len_arg))
-for i, j in enumerate(sys.argv[1:]):
-    print("{}: {}".format(i, j))
+if __name__ == "__main__":
+    import sys
+    len_arg = len(sys.argv) - 1
+    if len_arg == 0:
+        print("{} argument.".format(len_arg))
+    elif len_arg == 1:
+        print("{} argument:".format(len_arg))
+    else:
+        print("{} arguments:".format(len_arg))
+    for i, j in enumerate(sys.argv[1:]):
+        print("{}: {}".format(i, j))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-suma = 0
-for i, j in enumerate(sys.argv[1:]):
-    suma += int(j)
-    if i == len(sys.argv) - 2:
-        print("{}".format(suma))
+if __name__ == "__main__":
+    import sys
+    suma = 0
+    for i, j in enumerate(sys.argv[1:]):
+        suma += int(j)
+        if i == len(sys.argv) - 2:
+            print("{}".format(suma))
