fd54509513d6b84139dc701a039f3124067a1316
apla02 <1873@holbertonschool.com>
2020-09-10 16:59:55 -0500

    weight average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    score, average, weight = 0, 0, 0
+    if my_list:
+        for i, j in my_list:
+            score += i * j
+            average += j
+        resultado = score /average
+    return resultado
