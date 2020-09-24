0257ba7d043f971a3373ef08d541c7940e0e87b4
apla02 <1873@holbertonschool.com>
2020-09-24 15:42:49 -0500

    new version

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+function to print a full name with two strings
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """prints a full name
+
+    Args:
+        first_name (str): first name
+        last_name (str): last name. Defaults to "".
+
+    Raises:
+        TypeError: "first_name must be a string"
+        TypeError: "last_name must be a string"
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
