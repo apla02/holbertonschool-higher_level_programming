7685b978afb15cc044fbc6ac4665ebbb88ff3f3b
apla02 <1873@holbertonschool.com>
2020-10-06 15:47:43 -0500

    added

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class MyInt(int):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __eq__(self, other):
+        return self.value != other
+
+    def __ne__(self, other):
+        return self.value == other
