72fb53a5e2a47dffdbd9c90d87b7f4104c01a16e
apla02 <1873@holbertonschool.com>
2020-10-07 15:18:50 -0500

    dictionaries

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+    class of Student
+'''
+
+
+class Student:
+    '''
+    represents a Student class
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''
+        instantiation of attributes of instance
+        args:
+            name: name of stdudent
+            last_name: last name of student
+            age: age of student
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        method to retrieves the dictionary repr of a class
+        '''
+        dictionary = {}
+        if type(attrs) == list:
+            for i in attrs:
+                if type(i) is str and i in self.__dict__.keys():
+                    dictionary[i] = self.__dict__[i]
+                    return dictionary
+        return self.__dict__
