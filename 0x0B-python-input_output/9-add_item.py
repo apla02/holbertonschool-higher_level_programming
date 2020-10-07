64a8cb1be26f97a9d029d0c08941dbcf33cdacc9
apla02 <1873@holbertonschool.com>
2020-10-07 17:13:28 -0500

    add

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+'''
+    module to serialize and object
+'''
 import json
 import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-'''
-    module to serialize and object
-'''
+
 if __name__ == "__main__":
     try:
         my_list = load_from_json_file("add_item.json")
