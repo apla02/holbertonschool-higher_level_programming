074e2a60fbd8449bd0c2b86551969eee9e77688c
apla02 <1873@holbertonschool.com>
2020-10-13 16:10:16 -0500

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,6 +7,7 @@ from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 import inspect
+import pep8
 
 
 class Test_base(unittest.TestCase):
@@ -54,13 +55,13 @@ class Test_base(unittest.TestCase):
         b6 = Base([1, 2, 3])
         self.assertEqual(b6.id, [1, 2, 3])
 
-
-
-    def test_More_Arguments(self):
-        '''
-            test to check more argumments passed an Error
-        '''
-        self.assertRaises(TypeError, self.id, (1, 2))
+    def test_pep8(self):
+        """
+        Test for PEP-8
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["models/base.py"])
+        self.assertEqual(result.total_errors, 0)
 
     if __name__ == '__main__':
         unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -4,27 +4,47 @@
 '''
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
+from models.rectangle import Rectangle, __doc__
 from models.square import Square
+import inspect
+import pep8
 
 
 class Test_Rectangle_Class(unittest.TestCase):
     '''
         test to check the Rectangle Class
     '''
-    def test_Rectangle_Id_Nopassed(self):
+    def test_documentation(self):
+        '''
+        checking the documantation
+        '''
+        self.assertTrue(len(__doc__.strip()) > 0)
+        self.assertTrue(len(Base.__doc__.strip()) > 0)
+        functions = inspect.getmembers(Base, predicate=inspect.ismethod)
+        for name, func in functions:
+            self.assertTrue(len(func.__doc__.strip()) > 0)
+        functions = inspect.getmembers(Base, predicate=inspect.isfunction)
+        for name, func in functions:
+            self.assertTrue(len(func.__doc__.strip()) > 0)
+
+    def test_Rectangle_valid(self):
         '''
             test to check id no passed, and weight y height passed
         '''
         r1 = Rectangle(10, 2)
+        self.assertEqual(10, r1.width)
+        self.assertEqual(2, r1.height)
+        self.assertEqual(0, r1.x)
+        self.assertEqual(0, r1.y)
         self.assertEqual(3, r1.id)
 
-    def test_Rectangle_Arguments_And_Id(self):
-        '''
-            test to check id, weight, and height passed
-        '''
-        r1 = Rectangle(10, 2, 5)
-        self.assertEqual(5, r1.id)
+    def test_pep8(self):
+        """
+        Test for PEP-8
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["models/rectangle.py"])
+        self.assertEqual(result.total_errors, 0)
 
     def test_Rectangle_Arguments_And_Id(self):
         '''
@@ -47,14 +67,11 @@ class Test_Rectangle_Class(unittest.TestCase):
         self.assertRaises(TypeError, self.id, (-2, 2, 0, 0))
         self.assertRaises(TypeError, self.id, (0, 1, -1, 0))
 
-
     def test_Rectangle_More_Args(self):
         '''
             test more arguments
         '''
         self.assertRaises(TypeError, self.id, (2, 1, "str", 0, (2, 2), 5))
-        
-
 
 if __name__ == '__main__':
     unittest.main()
