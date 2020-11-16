fb536a339c69851899114a9ff47c8ac653a09b6b
apla02 <1873@holbertonschool.com>
2020-11-16 17:15:28 -0500

    tasks added

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- that converts hbtn_0c_0 database to UTF8
-ALTER TABLE hbtn_0c_0
-.second_table
+USE hbtn_0c_0
+ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
