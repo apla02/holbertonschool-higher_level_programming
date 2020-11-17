4ccc84581a8a417ec93cf7d404a7d5763761c9ce
apla02 <1873@holbertonschool.com>
2020-11-17 13:57:07 -0500

    using natural join to retrieve all cities into of database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa.
-SELECT id, name
+SELECT cities.id, cities.name, states.name
 FROM cities
-NATURAL JOIN states
-ORDER BY cities.id ASC;
+INNER JOIN states
+WHERE cities.states_id = states.id;
