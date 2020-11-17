a853eda1be7d8b41bd3ef85549af7331405c736a
apla02 <1873@holbertonschool.com>
2020-11-17 17:17:30 -0500

    all shows with gender linked

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,5 +5,5 @@ FROM tv_shows
 LEFT OUTER JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 LEFT OUTER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres .id
+ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
