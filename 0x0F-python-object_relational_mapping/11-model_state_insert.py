3ab0e2f87e2fbe3c26576506af3d6b22ff437d4d
apla02 <1873@holbertonschool.com>
2020-12-12 13:48:29 -0500

    issues pep8

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -17,9 +17,7 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    Session = sessionmaker()
-    Session.configure(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
     Lousiana = State(name='Lousiana')
     session.add(Lousiana)
