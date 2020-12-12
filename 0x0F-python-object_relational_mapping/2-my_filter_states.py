8961272e32d7481e508185d0e565726811458f59
apla02 <1873@holbertonschool.com>
2020-12-12 12:28:29 -0500

    changed line 17

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states "
-                " WHERE name LIKE '{:s}' ORDER BY id ASC ".
+                " WHERE name LIKE '{:s}' ORDER BY states.id ASC ".
                 format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
