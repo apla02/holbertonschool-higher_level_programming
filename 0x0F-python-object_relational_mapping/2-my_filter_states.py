7b33fca238b90661a3f26e5b4553a81957e49964
apla02 <1873@holbertonschool.com>
2020-12-12 17:12:33 -0500

    missing condition

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=argv[1], passwd=argv[2],
         db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' "
+                "ORDER BY states.id ASC ".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         if row[1] == argv[4]:
