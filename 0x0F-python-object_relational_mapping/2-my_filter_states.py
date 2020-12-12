b63fd1d999b2c25fb8d9a8bb1546e24388262fc6
apla02 <1873@holbertonschool.com>
2020-12-12 12:57:40 -0500

    checnge the option like by index in for with  argv[4]

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,21 +3,19 @@
 lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa
 """
+import MySQLdb
+from sys import argv
 
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
-
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2],
-        db=argv[3], charset="utf8")
+        db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states "
-                " WHERE name LIKE '{:s}' ORDER BY states.id ASC ".
-                format(argv[4]))
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     db.close()
