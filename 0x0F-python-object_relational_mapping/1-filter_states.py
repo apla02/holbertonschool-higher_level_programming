8684f47f655fd894149d7a3c847dbd1a93588ff1
apla02 <1873@holbertonschool.com>
2020-12-12 12:53:32 -0500

    checker doesnt like like condition

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=argv[1], passwd=argv[2],
         db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' "
-                "ORDER BY states.id ASC ")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
     db.close()
