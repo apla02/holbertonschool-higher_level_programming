2df33cb0d14a6bab64d76a46df561b988e3355f6
apla02 <1873@holbertonschool.com>
2020-12-12 16:36:16 -0500

    prints all City objects from the database hbtn_0e_14_usa:

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,19 +2,18 @@
 """
 class definition of a State and an instance Base = declarative_base()
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """
-
-    Args:
-        Base ([type]): [description]
+    Class City
     """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
