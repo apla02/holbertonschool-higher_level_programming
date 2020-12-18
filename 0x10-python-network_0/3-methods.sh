86f746b0255949a4e5de36bd53e388bbec80336c
apla02 <1873@holbertonschool.com>
2020-12-18 14:18:43 -0500

    show the methos allowed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI 0.0.0.0:5000 -X options |grep -i Allow |cut -d " " -f 2-                                                               
+curl -sI "$1" -X options |grep -i Allow |cut -d " " -f 2-                                                               
