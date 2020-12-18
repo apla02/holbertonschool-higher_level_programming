8218be0f0cf2335ba9be5c41fc1287d4f21a7001
apla02 <1873@holbertonschool.com>
2020-12-18 14:07:48 -0500

    display the body of response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,4 +2,4 @@
 # script Bash script that takes in a URL, sends a GET
 # request to the URL, and displays the body of the response
 # script take -X GET by default, s to silent mode, L follow redirects
-curl -sL "$1"
+curl -sLf "$1"
