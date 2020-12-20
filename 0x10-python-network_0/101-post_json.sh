db94e366a6ffee49b73dea9a38d5daf2134f80e0
apla02 <1873@holbertonschool.com>
2020-12-20 15:20:18 -0500

    curl post to json file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# using curl with post to json file
-curl -X POST -H "Content-Type: application/json" -d "$2" @"$1"
+# using curl with post to json
+curl -X POST -H "Content-Type: application/json" -d @"$2" "$1"
