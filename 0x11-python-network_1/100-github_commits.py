86bf5b724bb4ef40c31a643c718f23b47e983b2f
apla02 <apla02@gmail.com>
2021-01-11 13:00:52 -0500

    filtering commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Python script that takes your Github credentials
+(username and password) and uses the Github API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        owner_name, repository_name)
+    response = requests.get(url)
+    response_json = response.json()
+    try:
+        for i in range(10):
+            print("{} {}".format(response_json[i].get(
+                "sha"), response_json[i].get(
+                    "commit").get("author").get("name")))
+    except:
+        pass
