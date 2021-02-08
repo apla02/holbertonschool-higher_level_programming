e57a082f7cd10386deed578839b03673fa34a5fa
apla02 <apla02@gmail.com>
2021-02-08 14:04:26 -0500

    fixing map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,6 +4,6 @@ script that imports an array and computes a new array
 */
 
 const list = require('./100-data.js').list;
-const list2 = list.map(x => x * list.indexOf(x));
 console.log(list);
+const list2 = list.map((x, index) => x * index);
 console.log(list2);
